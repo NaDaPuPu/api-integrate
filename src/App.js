087a8a10dcd773a6f8ec8917e63498e9c3ea1a21
NaDaPuPu <jun19998@gmail.com>
2021-04-01 17:22:38 +0900

    Context + 비동기 API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Users from './Users';
+import { UsersProvier } from './UsersContext';
 
 function App() {
-  return <Users />;
+  return (
+    <UsersProvier>
+      <Users />
+    </UsersProvier>
+  );
 }
 
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import axios from 'axios';
-import useAsync from './useAsync';
-
-async function getUser(id) {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
-}
+import React, { useEffect } from 'react';
+import { useUsersState, useUsersDispatch, getUser } from './UsersContext';
 
 function User({ id }) {
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+  useEffect(() => {
+    // dispatch, id 값이 바뀔 때마다 getUser 함수 호출
+    getUser(dispatch, id);
+  }, [dispatch, id]);
+
+  const { data: user, loading, error } = state.user;
 
   if (loading) return <div>로딩 중...</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
